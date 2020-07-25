9b5bf6e3b4fa69e71da4196b36694244443cbc1b
Vaibhav Singh <vaibhavpratapsingh22@gmail.com>
2020-07-25 17:13:05 +0530

    Update 4-challenge-1.c

--- 3-algorithms/3-greedy-method/4-challenge-1.c
+++ 3-algorithms/3-greedy-method/4-challenge-1.c
@@ -4,7 +4,7 @@
 
 int main()
 {
-    int coins[] = {1,2,5,10,20,50,100,500,1000,5000,10000,100000,1000000};
+    int coins[] = {1,2,5,10,20,50,100,200,500,2000};
     int size = sizeof(coins)/sizeof(coins[0]);
     printf("Array Size is::%d\n",size);
     int value, i;
