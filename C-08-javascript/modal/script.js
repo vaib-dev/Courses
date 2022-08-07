55ed5cb21a09d6843bde5d377275961a6c7b96eb
vaibhav singh <vaibhavpratapsingh22@gmail.com>
2022-08-07 14:20:42 +0530

    Learning Js
    Created a modal using JavaScript and the next push will be for guess the number game. So be ready for for learning JS.

---
+++ C-08-javascript/modal/script.js
@@ -0,0 +1,22 @@
+let display = document.querySelectorAll(".show-modal");
+let data= document.querySelector('.modal p');
+let modal = document.querySelector('.modal');
+let close = document.querySelector('.close-modal');
+let overlay = document.querySelector('.overlay');
+console.log(overlay);
+console.log(display);
+
+for (let i = 0; i < display.length; i++) {
+  display[i].addEventListener("click", function () {
+    console.log(modal.classList);
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+
+  });
+}
+
+console.log(close);
+close.addEventListener('click', function(){
+modal.classList.add('hidden');
+overlay.classList.add('hidden');
+});
