d924c4001d806c104e3bf8a24b56a86bd343efd9
vaibhav singh <vaibhavpratapsingh22@gmail.com>
2022-08-07 18:57:23 +0530

    Guess the Number
    Here you will have to enter number between 0 and 20

---
+++ C-08-javascript/guess_my-number/script.js
@@ -0,0 +1,51 @@
+let numberEntered = Number(document.querySelector('.guess').value);
+let message = document.querySelector('.message').textContent;
+let score =20;
+let highScore = Number(document.querySelector('.highscore').textContent);
+let correctNumber= Math.floor(Math.random() * 20)+1;
+let check = document.querySelector('.check');
+let again = document.querySelector('.again');
+console.log(score);
+check.addEventListener('click',function(){
+
+console.log( typeof numberEntered);
+if(!numberEntered){
+document.querySelector('.message').textContent="HELLLOOOO";
+
+}
+if(numberEntered < correctNumber){
+document.querySelector('.message').textContent="Enter High Number";
+
+if(score<=0){
+    document.querySelector('.message').textContent="You lost";
+}else{
+    score--;
+}
+document.querySelector('.score').textContent=score;
+}
+if(numberEntered>correctNumber){
+    document.querySelector('.message').textContent="Enter Small Number";    
+    if(score<=0){
+        document.querySelector('.message').textContent="You lost";
+    }else{
+        score--;
+    }
+    document.querySelector('.score').textContent=score;
+}
+if(numberEntered==correctNumber){
+    document.querySelector('.message').textContent="You WON!!!!";
+document.querySelector('.highscore').textContent = correctNumber;
+document.querySelector('.number').textContent=correctNumber
+}
+});
+
+again.addEventListener('click',function(){
+    document.querySelector('.number').textContent="?";
+    score=20;
+    document.querySelector('.score').textContent=score;
+    document.querySelector('.guess').value = '';
+    document.querySelector('.message').textContent= "Start guessing...";
+    correctNumber= Math.floor(Math.random() * 20)+1;
+
+
+});
